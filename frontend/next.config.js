d60eb8b95bb43eb8151ebdee9c574b5b33eb2a9c
juin925 <juin925@naver.com>
2026-02-27 14:41:00 +0900

    api path fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${apiProxyTarget}/:path*`,
+        destination: `${apiProxyTarget}/api/:path*`,
       },
     ];
   },
